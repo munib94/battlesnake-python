66abd7d2afd8689ea43256a81c8f6366b8c04f81
munib94 <munib94@gmail.com>
2024-03-14 04:48:39 -0400

    Add A* search implementation

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import random
 import typing
 import sys
 
+from a_star import a_star_search
+
 
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
@@ -44,50 +46,31 @@ def end(game_state: typing.Dict):
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
+    # Get list of food nodes
+    foods = game_state["board"]["food"]
+    start = game_state["you"]["head"]
 
-    is_move_safe = {"up": True, "down": True, "left": True, "right": True}
-
-    # We've included code to prevent your Battlesnake from moving backwards
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
-        is_move_safe["left"] = False
-
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
-        is_move_safe["right"] = False
-
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
-        is_move_safe["down"] = False
-
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
-        is_move_safe["up"] = False
-
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
-
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
-
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
-
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
-
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
+    possible_moves = []
+    f_scores = []
+    # Perform A* search for all foods
+    for food in foods:
+        result = a_star_search(game_state, start, food)
+        if result is not None:
+            possible_moves.append(result[0])
+            f_scores.append(result[1])
+    
+    # Get lowest f_score and corresponding move
+    best_f_score = min(f_scores)
+    best_move = possible_moves[f_scores.index(best_f_score)]
+    row, col = best_move[0] - start["x"], best_move[1] - start["y"]
+    if row == 1:
+        next_move = "down"
+    if row == -1:
+        next_move = "up"
+    if col == 1:
+        next_move = "right"
+    if col == -1:
+        next_move = "left"
 
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
@@ -95,10 +78,16 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
 # Start server when `python main.py` is run
 if __name__ == "__main__":
-    from server import run_server
-    port = "8000"
-    for i in range(len(sys.argv) - 1):
-        if sys.argv[i] == '--port':
-            port = sys.argv[i+1]
 
-    run_server({"info": info, "start": start, "move": move, "end": end, "port": port})
+    from server import run_server
+
+    # Run on official server
+    run_server({"info": info, "start": start, "move": move, "end": end})
+
+    # Run on local server
+    # port = "8000"
+    # for i in range(len(sys.argv) - 1):
+    #     if sys.argv[i] == '--port':
+    #         port = sys.argv[i+1]
+
+    # run_server({"info": info, "start": start, "move": move, "end": end, "port": port})
